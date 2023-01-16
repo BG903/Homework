4307c70b02472ae6985ead05328f69d3cd7db7c8
BG903 <122629435+BG903@users.noreply.github.com>
2023-01-16 23:28:08 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,178 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TicTacToe
+namespace Homework
 {
-    enum Tick
+    internal class Program
     {
-        Empty,
-        X,
-        O
-    }
-
-    class Program
-    {
-        const char EMPTY = '-';
-
-        public static void Main()
+        static void Main(string[] args)
         {
-            byte firstPLayerCount = 0, seconfPlayerCount = 0;
-
-            while (true)
+            Random random = new Random();
+            int[,] matrix = new int[5, 5];
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                Console.WriteLine($"X - {firstPLayerCount}  :  O - {seconfPlayerCount}");
-                byte playCount = 0;
-                Tick[,] grid = new Tick[3, 3];
-                FillGrid(grid, Tick.Empty);
-                PrintGrid(grid);
-
-                bool isFirstPlayer = true;
-
-                Tick firstPlayer = Tick.X;
-                Tick secondPlayer = Tick.O;
-                bool isFinished = false;
-                while (true)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    isFinished = SetTick(grid, isFirstPlayer ? firstPlayer : secondPlayer);
-                    PrintGrid(grid);
-                    playCount++;
-                    if (isFinished || playCount == 9)
-                        break;
-                    isFirstPlayer = !isFirstPlayer;
+                    matrix[i, j] = random.Next(10, 100);
+                    Console.Write($"{matrix[i, j]}, ");
                 }
-
-
-                if (isFinished)
-                {
-                    if (isFirstPlayer)
-                        firstPLayerCount++;
-                    else
-                        seconfPlayerCount++;
-                }
-
-                Console.Clear();
-
-                if (playCount == 9 && !isFinished)
-                    Console.WriteLine("Draw");
-                else
-                    Console.WriteLine("Winner is {0}", isFirstPlayer ? firstPlayer : secondPlayer);
-
-            }
-
-        }
-
-        static void PrintGrid(Tick[,] grid)
-        {
-            for (int i = 0; i < grid.GetLength(0); i++)
-            {
-                for (int j = 0; j < grid.GetLength(1); j++)
-                {
-                    if (grid[i, j] == Tick.Empty)
-                    {
-                        Console.Write("{0}\t", EMPTY);
-                    }
-                    else
-                    {
-                        Console.Write("{0}\t", grid[i, j]);
-                    }
-                }
-
                 Console.WriteLine();
             }
-        }
-
-        static void FillGrid(Tick[,] grid, Tick tick)
-        {
-            for (int i = 0; i < grid.GetLength(0); i++)
+            int sum = 0, count = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 0; j < grid.GetLength(1); j++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    grid[i, j] = tick;
+                    if (j >= i)
+                    {
+                        sum += matrix[i, j];
+                        count++;
+                    }
                 }
             }
-        }
+            Console.Write("1)");
+            Console.WriteLine("Answer = {0}", sum / count);
+            int count2 = 0;
 
-        static Tick GetTick()
-        {
-            string input;
-            do
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                Console.Write("Please input your tick: ");
-                input = Console.ReadLine();
-            } while (input.Length > 1 || input.ToUpper() != Tick.X.ToString() || input.ToUpper() != Tick.O.ToString());
-
-            return input.ToUpper() == Tick.X.ToString() ? Tick.X : Tick.O;
-        }
-
-        static bool SetTick(Tick[,] grid, Tick tick)
-        {
-            string[] inputCoords;
-            int i;
-            int j;
-            do
-            {
-                Console.Write("Input Coord (i,j): ");
-                string input = Console.ReadLine();
-                inputCoords = input.Split(',');
-            } while (!int.TryParse(inputCoords[0], out i) || !int.TryParse(inputCoords[1], out j) ||
-                     grid[i, j] != Tick.Empty);
-
-            grid[i, j] = tick;
-
-            if (CheckHorizontal(grid, tick, i, j) || CheckVertical(grid, tick, i, j) || ChecktDiagonal(grid, tick, i, j))
-            {
-                return true;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i + j <= matrix.GetLength(0) - 1)
+                    {
+                        if (matrix[i, j] % 2 != 0)
+                        {
+                            count2++;
+                        }
+                    }
+                    Console.Write(matrix[i, j] + ", ");
+                }
+                Console.WriteLine();
             }
-            return false;
-        }
-
-
-        static bool CheckHorizontal(Tick[,] grid, Tick tick, int i, int j)
-        {
-            for (int k = 0; k < grid.GetLength(1); k++)
-            {
-                if (k == j)
-                {
-                    continue;
-                }
-
-                if (grid[i, k] != tick)
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        static bool CheckVertical(Tick[,] grid, Tick tick, int i, int j)
-        {
-            for (int k = 0; k < grid.GetLength(0); k++)
-            {
-                if (k == i)
-                {
-                    continue;
-                }
-
-                if (grid[k, j] != tick)
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        static bool ChecktDiagonal(Tick[,] grid, Tick tick, int i, int j)
-        {
-            if (grid[0, 0] != Tick.Empty || grid[2, 2] != Tick.Empty)
-                if (grid[0, 0] == grid[1, 1] && grid[2, 2] == grid[1, 1])
-                    return true;
-            if (grid[0, 2] != Tick.Empty || grid[2, 0] != Tick.Empty)
-                if (grid[2, 0] == grid[1, 1] && grid[0, 2] == grid[1, 1])
-                    return true;
-
-            return false;
+            Console.Write("2)");
+            Console.WriteLine("Answer = {0}", count2);
         }
     }
 }
